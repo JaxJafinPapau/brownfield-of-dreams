d4ed885f61c725fa9d1f524d0ec320216773edbd
Ethan Grab <ethangrab@gmail.com>
2019-05-08 09:37:50 -0600

    github service for github api call

---
+++ app/services/github_service.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class GithubService
+
+  def github_info
+    get_json('/user/repos')
+  end
+
+  private
+
+  def get_json(_url)
+    response = conn.get(_url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://api.github.com') do |f|
+      f.adapter Faraday.default_adapter
+      f.authorization :Bearer, '93b1d005bcfc93c0683486c7b04bbb6872872f73'
+      #f.params[:key] = current_user.github_key
+      # f.token_auth('93b1d005bcfc93c0683486c7b04bbb6872872f73')
+    end
+  end
+end
