a7e3acd3e4979ff9d03bf40d494f36d041eec177
Ethan Grab <ethangrab@gmail.com>
2019-05-14 10:37:11 -0600

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :friends, only: [:create]
 
   get '/dashboard', to: 'users#show'
   get '/auth/github/callback', to: 'users#update'
