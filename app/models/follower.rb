ed270d05ab163611713057c1d72bc0af1c91d60a
Ethan Grab <ethangrab@gmail.com>
2019-05-14 10:36:11 -0600

    follower has account method

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,7 +1,13 @@
 class Follower
-  attr_reader :handle, :url
+  attr_reader :handle, :url, :github_id
   def initialize(data)
     @handle = data[:login]
     @url  = data[:html_url]
+    @github_id = data[:id]
+  end
+
+
+  def follower_has_account?(user)
+    User.exists?(github_id: self.github_id) && !user.friends.exists?(github_id: self.github_id)
   end
 end
