511d12f103ee816cbd09bf195483385ef291dbdf
Ethan Grab <ethangrab@gmail.com>
2019-05-17 11:48:42 -0600

    Invitations controller to handle github invitation email and api call

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,25 @@
+class InvitationsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    # github_email = github_data[:email]
+    github_email = 'test@gmail.com'
+    UserMailer.github_invite(current_user, github_email).deliver_now
+    flash[:notice] = "Successfully sent invite!"
+    redirect_to root_url
+  end
+
+  private
+
+  def github_data
+    @_github_data ||= service.github_info(params[:invitation][:github_handle])
+  end
+
+  def service
+    @_service ||= GithubService.new
+  end
+
+
+end
