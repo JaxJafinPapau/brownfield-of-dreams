2836c99c4517273d9771b031a3a33bf824c1954a
jaxjafinpapau <25406708+JaxJafinPapau@users.noreply.github.com>
2019-05-18 00:45:13 -0700

    refactors welcome_controller index logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,12 +6,10 @@ class WelcomeController < ApplicationController
   def index
     if params[:tag]
       @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+    elsif current_user.nil?
+      @tutorials = Tutorial.all.where(classroom: false).paginate(page: params[:page], per_page: 5)
     else
-      if current_user.nil?
-        @tutorials = Tutorial.all.where(classroom: false).paginate(page: params[:page], per_page: 5)
-      else
-        @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-      end
+      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
     end
   end
   # rubocop:enable Style/ConditionalAssignment
